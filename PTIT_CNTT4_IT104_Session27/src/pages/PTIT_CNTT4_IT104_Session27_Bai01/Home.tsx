f04e7ee2f9942ba39d0cd2b5a1f4a875a1f0c15a
letrungchien <letrungchien195@gmail.com>
2025-09-11 21:36:13 +0700

    update

---
+++ PTIT_CNTT4_IT104_Session27/src/pages/PTIT_CNTT4_IT104_Session27_Bai01/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Chào mừng đến với ứng dungj giới thiệu bản thân</h1>
+        <p>đây là ứng dụng đơn giản</p>
+    </div>
+  )
+}
---
+++ PTIT_CNTT4_IT104_Session27/src/pages/PTIT_CNTT4_IT104_Session27_Bai02/Bai2.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Bai2() {
+  return (
+    <div>Bai2</div>
+  )
+}
