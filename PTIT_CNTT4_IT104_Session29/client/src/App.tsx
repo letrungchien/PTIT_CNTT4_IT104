e0321188e95aeb99f96ccaaba1a216562e764c4b
letrungchien <letrungchien195@gmail.com>
2025-09-16 05:51:12 +0700

    update

---
+++ PTIT_CNTT4_IT104_Session29/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+ import Student from "./componets/Student";
+ import Product from './componets/Product';
+export default function App() {
+  return (
+    <>
+    {/* bai1 + bai2 */}
+    <Product/>
+    <hr></hr>
+    {/* tu bai3 den bai 8 */}
+    <Student/>
+    </>
+  )
+}
