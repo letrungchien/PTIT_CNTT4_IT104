b1b5c3191f58f80019a9d3a60d8f87ad5e346420
letrungchien <letrungchien195@gmail.com>
2025-09-16 23:37:29 +0700

    update

---
+++ PTIT_CNTT4_IT104_Session30/client/vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Job from "./components/Job";
+export default function App() {
+
+  return (
+    <div>
+      {/* 8 bai dau */}
+      <Job/>
+    </div>
+  )
+}
